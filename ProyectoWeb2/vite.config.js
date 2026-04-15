333c18d01de309413f95cdf1b253a3c5f7493677
jimmyferc10 <jimmyferc10@gmail.com>
2026-04-15 10:59:08 -0600

    Haciendo Deploy

--- ProyectoWeb2/vite.config.js
+++ ProyectoWeb2/vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from "vite";
-import path, { resolve } from "node:path";
-import { glob } from "glob";
+import { resolve } from "node:path";
+import pkg from "glob";
 import HtmlCssPurgePlugin from "vite-plugin-purgecss";
 import HandlebarsPlugin from "vite-plugin-handlebars";
 
 function obtenerHtmlFiles() {
+   
+    const files = (pkg.sync || pkg.globSync)('./**/*.html', {
+        ignore: [
+            './dist/**',
+            './node_modules/**'
+        ]
+    });
+
     return Object.fromEntries(
-        glob.sync('./**/*.html', {
-            ignore: [
-                './dist/**',
-                './node_modules/**'
-            ]
-        }).map((file) => {
+        files.map((file) => {
             return [
                 file.replace(/\.html$/, '').replace(/^\.\//, ''),
                 resolve(__dirname, file)
@@ -22,6 +25,7 @@ function obtenerHtmlFiles() {
 
 export default defineConfig({
     appType: 'mpa',
+    base: process.env.DEPLOY_BASE_URL || '/',
     build: {
         rollupOptions: {
             input: obtenerHtmlFiles(),
